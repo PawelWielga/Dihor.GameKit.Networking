fdb5925831c3657e53db698e00924a59eeed6f3c
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-12 21:57:33 +0200

    [14] Exercise public types from packaged assemblies

---
+++ packaging/consumer/Program.cs
@@ -0,0 +1,13 @@
+using PartyGameKit.Core;
+using PartyGameKit.Discovery.Lan;
+using PartyGameKit.Protocol;
+using PartyGameKit.Transport.InMemory;
+using PartyGameKit.Transport.Lan;
+
+var roomId = new RoomId("package-smoke-room");
+var joinCode = new JoinCode("SMOKE1");
+
+Console.WriteLine($"{roomId.Value}:{joinCode.Value}:v{ProtocolVersions.Current}");
+Console.WriteLine(typeof(InMemoryGameTransport).FullName);
+Console.WriteLine(typeof(LanWebSocketTransport).FullName);
+Console.WriteLine(typeof(UdpLanDiscoveryAdvertiser).FullName);
