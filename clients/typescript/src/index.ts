b24821099a7ca17bb1d39aaafebc4e65bdd0253f
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-12 22:10:52 +0200

    [15] Export browser SignalR adapter

--- clients/typescript/src/index.ts
+++ clients/typescript/src/index.ts
@@ -2,4 +2,5 @@ export * from "./client.js";
 export * from "./identity.js";
 export * from "./join-descriptor.js";
 export * from "./protocol.js";
+export * from "./signalr.js";
 export * from "./snapshot.js";
