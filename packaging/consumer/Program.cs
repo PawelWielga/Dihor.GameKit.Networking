2c3829a73259093d8e46af8479f502453d0aefdf
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-12 22:16:29 +0200

    [15] Exercise packaged SignalR public surface

--- packaging/consumer/Program.cs
+++ packaging/consumer/Program.cs
@@ -3,6 +3,7 @@ using PartyGameKit.Discovery.Lan;
 using PartyGameKit.Protocol;
 using PartyGameKit.Transport.InMemory;
 using PartyGameKit.Transport.Lan;
+using PartyGameKit.Transport.SignalR;
 
 var roomId = new RoomId("package-smoke-room");
 var joinCode = new JoinCode("SMOKE1");
@@ -10,4 +11,5 @@ var joinCode = new JoinCode("SMOKE1");
 Console.WriteLine($"{roomId.Value}:{joinCode.Value}:v{ProtocolVersions.Current}");
 Console.WriteLine(typeof(InMemoryGameTransport).FullName);
 Console.WriteLine(typeof(LanWebSocketTransport).FullName);
+Console.WriteLine(typeof(SignalRRoomTransport).FullName);
 Console.WriteLine(typeof(UdpLanDiscoveryAdvertiser).FullName);
