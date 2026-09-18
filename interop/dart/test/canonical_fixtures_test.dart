83637da90759c7b572d883d1037693f25d16e2e0
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-18 15:14:20 +0200

    style: apply Dart 3.13 test formatting

--- interop/dart/test/canonical_fixtures_test.dart
+++ interop/dart/test/canonical_fixtures_test.dart
@@ -52,7 +52,8 @@ void main() {
 
       expect(descriptor.protocolVersion, 2);
       expect(descriptor.transport, 'lan-websocket');
-      expect(descriptor.endpoint, 'ws://192.168.1.10:45678/dihor-gamekit-networking');
+      expect(descriptor.endpoint,
+          'ws://192.168.1.10:45678/dihor-gamekit-networking');
       expect(descriptor.channelId, 'channel-a');
       expect(descriptor.toJsonString(), canonical);
 
