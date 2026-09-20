33dddc03380bb02909b63cf7f89d0a103d6473bb
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-20 08:56:25 +0200

    [27] Keep Dart error events as ordinary stream values

--- clients/dart/lib/src/client.dart
+++ clients/dart/lib/src/client.dart
@@ -324,7 +324,7 @@ final class DihorGameKitNetworkingClient {
 
   void _handleProtocolError(Object error, StackTrace stackTrace) {
     if (!_controllersClosed) {
-      _errorController.addError(error, stackTrace);
+      _errorController.add(error);
     }
 
     final pending = _pendingConnection;
@@ -335,7 +335,7 @@ final class DihorGameKitNetworkingClient {
 
   void _handleTransportError(Object error, StackTrace stackTrace) {
     if (!_controllersClosed) {
-      _errorController.addError(error, stackTrace);
+      _errorController.add(error);
     }
 
     final pending = _pendingConnection;
