ba638a8e12825a13192db86d9fc5b3de1d4cbd17
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-20 09:06:03 +0200

    [28] Export Dart continuity APIs

--- clients/dart/lib/dihor_gamekit_networking.dart
+++ clients/dart/lib/dihor_gamekit_networking.dart
@@ -1,5 +1,7 @@
 export 'package:dihor_gamekit_networking_protocol/dihor_gamekit_networking_protocol.dart';
 
 export 'src/client.dart';
+export 'src/identity.dart';
 export 'src/lan_websocket_transport.dart';
+export 'src/reconnect.dart';
 export 'src/transport.dart';
